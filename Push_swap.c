7ffa315f94abf6de720a460db9aa452efa958b17
andrestefane <163122983+andrestefane@users.noreply.github.com>
2024-10-18 15:15:42 +0200

    Update Push_swap.c

--- Push_swap.c
+++ Push_swap.c
@@ -11,3 +11,36 @@
 /* ************************************************************************** */
 
 #include <Push_swap.h>
+
+int	*ft_free(int **str)
+{
+	free(*str);
+	*str = NULL;
+	return (NULL);
+}
+
+t_stack_a	create_stack_a(int capacity)
+{
+	t_stack_a	*stack;
+
+	*stack = malloc(sizeof(t_stack_a));
+	if (!*stack)
+		stack = ft_free;
+	stack->collection = malloc(capacity * sizeof(int));
+	stack->capacity = capacity;
+	stack->size = 0;
+	return (stack);
+}
+
+t_stack_b	create_stack_b(int capacity)
+{
+	t_stack_b	*stack;
+
+	*stack = malloc(sizeof(t_stack_b));
+	if (!*stack)
+		stack = ft_free;
+	stack->collection = malloc(capacity * sizeof(int));
+	stack->capacity = capacity;
+	stack->size = 0;
+	return (stack);
+}
